5444aca77e4278f96cb0c0c594fdf780b2577275
shrewman <feelswhiteman@gmail.com>
2024-04-11 13:48:56 +0300

    navigate and stage git hunks

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -9,6 +9,30 @@ return {
         '<leader>g',
         '<cmd>Gitsigns preview_hunk_inline<CR>',
         {}
+      );
+      vim.keymap.set(
+        'n',
+        '<leader>[',
+        '<cmd>Gitsigns prev_hunk<CR>',
+        {}
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>]',
+        '<cmd>Gitsigns next_hunk<CR>',
+        {}
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>hs',
+        '<cmd>Gitsigns stage_hunk<CR>',
+        {}
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>hu',
+        '<cmd>Gitsigns undo_stage_hunk<CR>',
+        {}
       )
     end,
   },
